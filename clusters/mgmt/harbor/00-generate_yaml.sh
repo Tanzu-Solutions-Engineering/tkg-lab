c508fd47553956941a8a415bbe1766992acf0f16
adamzwickey <azwickey@vmware.com>
2020-04-05 21:49:43 -0400

    added docs for harbor install

--- clusters/mgmt/harbor/00-generate_yaml.sh
+++ clusters/mgmt/harbor/00-generate_yaml.sh
@@ -11,7 +11,7 @@ echo "---" >> clusters/mgmt/harbor/generated/02-certs.yaml
 yq read -d1 clusters/mgmt/harbor/02-certs.yaml >> clusters/mgmt/harbor/generated/02-certs.yaml
 yq write -d0 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.acme.email" $EMAIL  
 yq write -d0 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.acme.solvers[0].dns01.clouddns.project" $PROJECT_ID 
-yq write -d1 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.commonName" $CN
+yq write -d1 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.commonName" $HARBOR_CN
 yq write -d1 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.dnsNames[0]" $HARBOR_CN
 yq write -d1 clusters/mgmt/harbor/generated/02-certs.yaml -i "spec.dnsNames[1]" $NOTARY_CN
 
