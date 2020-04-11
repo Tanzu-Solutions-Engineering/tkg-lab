531064aff40344bcbf210c3c06956d04286194f0
adamzwickey <azwickey@vmware.com>
2020-04-11 15:15:09 -0400

    Cleaned up a bit removing env specific info.  Also script depends on en vars VS. passed in var from CLI

--- scripts/01-prep-aws-objects.sh
+++ scripts/01-prep-aws-objects.sh
@@ -1,10 +1,11 @@
 # usage: ./01-prep-aws-objects.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
 
-export AWS_ACCESS_KEY_ID=$1
-export AWS_SECRET_ACCESS_KEY=$2
-export AWS_REGION=us-east-2
+#export AWS_ACCESS_KEY_ID=$1
+#export AWS_SECRET_ACCESS_KEY=$2
+#export AWS_REGION=us-east-2
 
 clusterawsadm alpha bootstrap create-stack
 
-aws ect delete-key-pair --key-name tkg-default
-aws ec2 create-key-pair --key-name tkg-default --output json | jq .KeyMaterial -r > keys/aws-ssh.pem.pem
+mkdir -p keys/
+aws ec2 delete-key-pair --key-name tkg-default
+aws ec2 create-key-pair --key-name tkg-default --output json | jq .KeyMaterial -r > keys/aws-ssh.pem
