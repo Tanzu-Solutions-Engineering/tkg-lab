8b33c0cba2a96aacad04d3acf0ddd97af996d711
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-04-30 10:07:04 -0400

    Adds check for sed differences in linux and mac

--- scripts/generate-fluent-bit-yaml-mgmt.sh
+++ scripts/generate-fluent-bit-yaml-mgmt.sh
@@ -5,7 +5,15 @@ mkdir -p clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elastic
 # 04-fluent-bit-configmap.yaml
 yq read tkg-extensions/logging/fluent-bit/aws/output/elasticsearch/04-fluent-bit-configmap.yaml > clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
 
-sed -i -e 's/<TKG_CLUSTER_NAME>/mgmt/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
-sed -i -e 's/<TKG_INSTANCE_NAME>/tkg-aws-lab/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
-sed -i -e 's/<FLUENT_ELASTICSEARCH_HOST>/elasticsearch/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
-sed -i -e 's/<FLUENT_ELASTICSEARCH_PORT>/9200/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+if [ `uname -s` = 'Darwin' ]; 
+then
+  sed -i '' -e 's/<TKG_CLUSTER_NAME>/mgmt/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i '' -e 's/<TKG_INSTANCE_NAME>/tkg-aws-lab/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i '' -e 's/<FLUENT_ELASTICSEARCH_HOST>/elasticsearch/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i '' -e 's/<FLUENT_ELASTICSEARCH_PORT>/9200/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+else
+  sed -i -e 's/<TKG_CLUSTER_NAME>/mgmt/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i -e 's/<TKG_INSTANCE_NAME>/tkg-aws-lab/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i -e 's/<FLUENT_ELASTICSEARCH_HOST>/elasticsearch/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+  sed -i -e 's/<FLUENT_ELASTICSEARCH_PORT>/9200/g' clusters/mgmt/tkg-extensions-mods/logging/fluent-bit/aws/output/elasticsearch/generated/04-fluent-bit-configmap.yaml
+fi
