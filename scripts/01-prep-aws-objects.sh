ab50489dd6b991560330a242cb243f6a83aa867a
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-05-01 19:07:18 -0400

    Updates management cluster aws scripts to allow for alternative regions

--- scripts/01-prep-aws-objects.sh
+++ scripts/01-prep-aws-objects.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 : ${AWS_ACCESS_KEY_ID?"Need to set AWS_ACCESS_KEY_ID environment variable"}
 : ${AWS_SECRET_ACCESS_KEY?"Need to set AWS_SECRET_ACCESS_KEY environment variable"}
+: ${AWS_REGION?"Need to set AWS_REGION environment variable"}
 
 clusterawsadm alpha bootstrap create-stack
 
