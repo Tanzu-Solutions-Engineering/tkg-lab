7ff743d6422d9853e7da157cb20b9a06fb456cb6
jaimegag <jaimegag@gmail.com>
2020-05-11 00:03:02 -0400

    Fix wlc contour + gangway for non AWS

--- scripts/generate-gangway-yaml.sh
+++ scripts/generate-gangway-yaml.sh
@@ -2,6 +2,7 @@
 
 : ${GANGWAY_CN?"Need to set GANGWAY_CN environment variable"}
 : ${DEX_CN?"Need to set DEX_CN environment variable"}
+: ${CLUSTER_NAME?"Need to set CLUSTER_NAME environment variable"}
 
 mkdir -p clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/generated
 
@@ -17,7 +18,7 @@ yq write -d0 clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/gener
 yq read tkg-extensions-mods-examples/authentication/gangway/aws/03-config.yaml > clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/generated/03-config.yaml
 sed -i '' -e 's/$DEX_CN/'$DEX_CN'/g' clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/generated/03-config.yaml
 sed -i '' -e 's/$GANGWAY_CN/'$GANGWAY_CN'/g' clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/generated/03-config.yaml
-WLC_1_API_SERVER_URL=`kubectl config view -o jsonpath='{.clusters[?(@.name=="wlc-1")].cluster.server}'`
+WLC_1_API_SERVER_URL=`kubectl config view -o jsonpath='{.clusters[?(@.name=="'${CLUSTER_NAME}'")].cluster.server}'`
 WLC_1_API_SERVER_CN=`cut -d ':' -f 2 <<< $WLC_1_API_SERVER_URL | cut -d '/' -f 3`
 sed -i '' -e 's/$WLC_1_API_SERVER_CN/'$WLC_1_API_SERVER_CN'/g' clusters/wlc-1/tkg-extensions-mods/authentication/gangway/aws/generated/03-config.yaml
 
