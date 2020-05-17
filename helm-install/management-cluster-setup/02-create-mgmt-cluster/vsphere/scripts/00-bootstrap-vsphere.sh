597cc6761d323ce965725a966bcdb56629dffe28
Jamal Khan <khan@Jamals-MacBook-Pro.local>
2020-05-17 05:10:38 -0400

    vsphere changes

--- helm-install/management-cluster-setup/02-create-mgmt-cluster/vsphere/scripts/00-bootstrap-vsphere.sh
+++ helm-install/management-cluster-setup/02-create-mgmt-cluster/vsphere/scripts/00-bootstrap-vsphere.sh
@@ -9,16 +9,37 @@ export GOVC_DATASTORE=$(yq r $PARAM_FILE vsphere.datastore)
 export GOVC_RESOURCE_POOL=$(yq r $PARAM_FILE vsphere.resourcepool)
 export GOVC_INSECURE=1
 export GOVC_FOLDER=$(yq r $PARAM_FILE vsphere.folder)
+export OVA_FOLDER=$(yq r $PARAM_FILE vsphere.ovafolder)
+export PHOTON_OVA=$(yq r $PARAM_FILE vsphere.phtonosova)
+export HAPROXY_OVA=$(yq r $PARAM_FILE vsphere.haproxyova)
 
-
-govc library.create -sub-ondemand=true -sub https://download3.vmware.com/software/vmw-tools/tkg-demo-appliance/cl/lib.json tkg-on-vmc-cl
-sleep 5s
+echo 'creating resource pool'
 govc pool.create $GOVC_RESOURCE_POOL
+echo 'creating folder'
 govc folder.create $GOVC_FOLDER
-govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_demo_vm_options.json /tkg-on-vmc-cl/TKG-Demo-Appliance_1.0.0 TKG-Demo-Appliance
-govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_photon_os_vm_options.json /tkg-on-vmc-cl/photon-3-v1.17.3_vmware.2
-govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_ha_proxy_vm_options.json /tkg-on-vmc-cl/photon-3-capv-haproxy-v0.6.3_vmware.1
 
+#echo 'importing ova'
+govc import.spec $OVA_FOLDER/$PHOTON_OVA | jq '.Name="'$PHOTON_OVA'"' | jq '.NetworkMapping[0].Network="'$GOVC_NETWORK'"' | jq '.MarkAsTemplate=true' > ./generated/$PHOTON_OVA.json
+govc import.ova -options=./generated/$PHOTON_OVA.json $OVA_FOLDER/$PHOTON_OVA
+
+govc import.spec $OVA_FOLDER/$HAPROXY_OVA | jq '.Name="'$HAPROXY_OVA'"' | jq '.NetworkMapping[0].Network="'$GOVC_NETWORK'"' | jq '.MarkAsTemplate=true'> ./generated/$HAPROXY_OVA.json
+govc import.ova -options=./generated/$HAPROXY_OVA.json $OVA_FOLDER/$HAPROXY_OVA
+
+#echo 'creating content library'
+#govc library.create -sub-ondemand=true -sub $CONTENT_LIBRARY_URL tkg-on-vmc-cl
+#echo 'creating resource pool'
+#govc pool.create $GOVC_RESOURCE_POOL
+#echo 'creating folder'
+#govc folder.create $GOVC_FOLDER
+#echo 'Deploying Demo appliance'
+#govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_demo_vm_options.json /tkg-on-vmc-cl/TKG-Demo-Appliance_1.0.0 TKG-Demo-Appliance
+#echo 'Creating photon template'
+#govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_photon_os_vm_options.json /tkg-on-vmc-cl/photon-3-v1.17.3_vmware.2
+#echo 'Creating haproxy template'
+#govc library.deploy -folder=$GOVC_FOLDER -options=./management-cluster-setup/02-create-mgmt-cluster/vSphere/lib/tkg_ha_proxy_vm_options.json /tkg-on-vmc-cl/photon-3-capv-haproxy-v0.6.3_vmware.1
+#echo 'bootstrapping done!'
+
+export PARAM_FILE=./params.yml
 #govc object.mv /Datacenter/vm/photon-3-v1.17.3+vmware.2 /Datacenter/vm/tkg
 #govc object.mv /Datacenter/vm/capv-haproxy-v0.6.2 /Datacenter/vm/tkg
 #govc snapshot.create -vm photon-3-v1.17.3+vmware.1 root
