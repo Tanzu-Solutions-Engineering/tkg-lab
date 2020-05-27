f9702388f403ae9374654f588a8130002a4b2124
Andrew Gregory <agregory@pivotal.io>
2020-05-27 20:27:48 +0000

    Wavefront Helm can be upgrade --install

--- scripts/deploy-wavefront.sh
+++ scripts/deploy-wavefront.sh
@@ -16,7 +16,7 @@ WAVEFRONT_PREFIX=$(yq r params.yaml wavefront.cluster-name-prefix)
 kubectl create namespace wavefront
 helm repo add wavefront https://wavefronthq.github.io/helm/
 helm repo update
-helm install wavefront wavefront/wavefront -f wavefront/wf.yml \
+helm upgrade --install wavefront wavefront/wavefront -f wavefront/wf.yml \
   --set wavefront.url=$WAVEFRONT_URL \
   --set wavefront.token=$WAVEFRONT_API_KEY \
   --set clusterName=$WAVEFRONT_PREFIX-$CLUSTER_NAME-$IAAS \
