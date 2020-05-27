be1b3aa8aeccac262c0070e9608e28150263cfda
agregory999 <agregory@pivotal.io>
2020-05-27 14:54:01 -0400

    Added IAAS to end of cluster name to aid in distinctiveness

--- scripts/tmc-policy.sh
+++ scripts/tmc-policy.sh
@@ -8,7 +8,8 @@ fi
 CLUSTER_NAME=$1
 ROLE=$2
 POLICY_GROUPS=$3
+IAAS=$(yq r params.yaml iaas)
 
 VMWARE_ID=$(yq r params.yaml vmware-id)
 
-tmc cluster iam add-binding $VMWARE_ID-$CLUSTER_NAME --role $ROLE --groups $POLICY_GROUPS
+tmc cluster iam add-binding $VMWARE_ID-$CLUSTER_NAME-$IAAS --role $ROLE --groups $POLICY_GROUPS
