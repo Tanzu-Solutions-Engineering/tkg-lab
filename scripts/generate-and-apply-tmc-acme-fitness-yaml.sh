d6885ad63f666d07a3daccea0c2b94cce99836ec
Andrew Gregory <agregory@pivotal.io>
2020-05-28 20:17:45 +0000

    Add IAAS to cluster name for distinctiveness

--- scripts/generate-and-apply-tmc-acme-fitness-yaml.sh
+++ scripts/generate-and-apply-tmc-acme-fitness-yaml.sh
@@ -8,16 +8,17 @@ CLUSTER_NAME=$1
 
 TMC_ACME_FITNESS_WORKSPACE_NAME=$(yq r params.yaml acme-fitness.tmc-workspace)
 VMWARE_ID=$(yq r params.yaml vmware-id)
+IAAS=$(yq r params.yaml iaas)
 
 mkdir -p generated/$CLUSTER_NAME/tmc
-cp -r tmc/config/ generated/$CLUSTER_NAME/tmc/
+cp -r tmc/config/* generated/$CLUSTER_NAME/tmc/
 
 # acme-fitness-dev.yaml
 yq write -d0 generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml -i "fullName.name" $TMC_ACME_FITNESS_WORKSPACE_NAME
 yq write -d0 generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml -i "objectMeta.labels.origin" $VMWARE_ID
 
 # tkg-wlc-acme-fitness.yaml
-yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "fullName.clusterName" $VMWARE_ID-$CLUSTER_NAME
+yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "fullName.clusterName" $VMWARE_ID-$CLUSTER_NAME-$IAAS
 yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "objectMeta.labels.origin" $VMWARE_ID
 yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "spec.workspaceName" $TMC_ACME_FITNESS_WORKSPACE_NAME
 
