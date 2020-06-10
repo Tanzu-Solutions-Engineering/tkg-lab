d63451d64ae3795421a6bcfad160d9a332070e55
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-06-10 14:20:50 -0400

    Adds OIDC Integraiton to Harbor and adjusts location of generated files

--- harbor/00-generate_yaml.sh
+++ harbor/00-generate_yaml.sh
@@ -2,22 +2,29 @@
 
 source ./scripts/set-env.sh
 
+if [ ! $# -eq 1 ]; then
+  echo "Must supply cluster_name as args"
+  exit 1
+fi
+
+CLUSTER_NAME=$1
+
 HARBOR_CN=$(yq r $PARAMS_YAML harbor.harbor-cn)
 NOTARY_CN=$(yq r $PARAMS_YAML harbor.notary-cn)
 
-mkdir -p harbor/generated/
+mkdir -p generated/$CLUSTER_NAME/harbor
 
 # 01-namespace.yaml
-yq read harbor/01-namespace.yaml > harbor/generated/01-namespace.yaml
+yq read harbor/01-namespace.yaml > generated/$CLUSTER_NAME/harbor/01-namespace.yaml
 
 # 02-certs.yaml
-yq read harbor/02-certs.yaml > harbor/generated/02-certs.yaml
-yq write harbor/generated/02-certs.yaml -i "spec.commonName" $HARBOR_CN
-yq write harbor/generated/02-certs.yaml -i "spec.dnsNames[0]" $HARBOR_CN
-yq write harbor/generated/02-certs.yaml -i "spec.dnsNames[1]" $NOTARY_CN
+yq read harbor/02-certs.yaml > generated/$CLUSTER_NAME/harbor/02-certs.yaml
+yq write generated/$CLUSTER_NAME/harbor/02-certs.yaml -i "spec.commonName" $HARBOR_CN
+yq write generated/$CLUSTER_NAME/harbor/02-certs.yaml -i "spec.dnsNames[0]" $HARBOR_CN
+yq write generated/$CLUSTER_NAME/harbor/02-certs.yaml -i "spec.dnsNames[1]" $NOTARY_CN
 
 # harbor-values.yaml
-yq read harbor/harbor-values.yaml > harbor/generated/harbor-values.yaml
-yq write harbor/generated/harbor-values.yaml -i "expose.ingress.hosts.core" $HARBOR_CN
-yq write harbor/generated/harbor-values.yaml -i "expose.ingress.hosts.notary" $NOTARY_CN  
-yq write harbor/generated/harbor-values.yaml -i "externalURL" https://$HARBOR_CN
+yq read harbor/harbor-values.yaml > generated/$CLUSTER_NAME/harbor/harbor-values.yaml
+yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "expose.ingress.hosts.core" $HARBOR_CN
+yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "expose.ingress.hosts.notary" $NOTARY_CN  
+yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "externalURL" https://$HARBOR_CN
