2b62af45cfc01da8a45fbec70ed498f5dcb80f51
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-06-25 15:00:42 -0400

    Changing the way sourcing set-env.sh is referenced

--- scripts/deploy-metallb.sh
+++ scripts/deploy-metallb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-source ./scripts/set-env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/set-env.sh
 
 if [ ! $# -eq 3 ]; then
   echo "Must supply cluster_name, metallb start and end ips as args"
