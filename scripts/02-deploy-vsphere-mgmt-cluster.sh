190191b05ce631c92b728cc4f9c45ef733fc1527
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-06-25 15:00:42 -0400

    Changing the way sourcing set-env.sh is referenced

--- scripts/02-deploy-vsphere-mgmt-cluster.sh
+++ scripts/02-deploy-vsphere-mgmt-cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-source ./scripts/set-env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/set-env.sh
 
 export MANAGEMENT_CLUSTER_NAME=$(yq r $PARAMS_YAML management-cluster.name)
 
