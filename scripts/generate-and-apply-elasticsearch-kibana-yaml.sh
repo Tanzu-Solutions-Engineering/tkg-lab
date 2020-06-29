76fa6b3cddfca4c9d5826d5e7fb03a3bcea05f7b
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-06-29 14:15:09 -0400

    Updates to make scripts callable from other PWDs

--- scripts/generate-and-apply-elasticsearch-kibana-yaml.sh
+++ scripts/generate-and-apply-elasticsearch-kibana-yaml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-source ./scripts/set-env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/set-env.sh
 
 ELASTICSEARCH_CN=$(yq r $PARAMS_YAML shared-services-cluster.elasticsearch-fqdn)
 KIBANA_CN=$(yq r $PARAMS_YAML shared-services-cluster.kibana-fqdn)
@@ -9,8 +10,8 @@ CLUSTER_NAME=$(yq r $PARAMS_YAML shared-services-cluster.name)
 kubectl config use-context $CLUSTER_NAME-admin@$CLUSTER_NAME
 
 mkdir -p generated/$CLUSTER_NAME/ek/
-cp elasticsearch-kibana/*.yaml generated/$CLUSTER_NAME/ek/
-cp elasticsearch-kibana/template/*.yaml generated/$CLUSTER_NAME/ek/
+cp $DIR/../elasticsearch-kibana/*.yaml generated/$CLUSTER_NAME/ek/
+cp $DIR/../elasticsearch-kibana/template/*.yaml generated/$CLUSTER_NAME/ek/
 
 yq write -d0 generated/$CLUSTER_NAME/ek/03b-ingress.yaml -i "spec.rules[0].host" $ELASTICSEARCH_CN
 yq write -d2 generated/$CLUSTER_NAME/ek/04-kibana.yaml -i "spec.rules[0].host" $KIBANA_CN
