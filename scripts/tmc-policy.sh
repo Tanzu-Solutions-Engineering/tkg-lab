aada6231788730917c75955ef4b4e54c0296112b
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-06-30 10:55:25 -0400

    Changing variable name for clarity

--- scripts/tmc-policy.sh
+++ scripts/tmc-policy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/set-env.sh
+TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $TKG_LAB_SCRIPTS/set-env.sh
 
 if [ ! $# -eq 3 ]; then
   echo "Must supply cluster name, role, and groups as args"
