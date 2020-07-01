1c2c23b9517d447fee520323b02b122037c0c541
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-07-01 14:15:09 -0400

    Revert "Add ability to more easily call tkg-lab scripts from other projects (#57)"
    This reverts commit d214144a8721e90a7b8d06978f785cd636d2683a.

--- scripts/tmc-attach.sh
+++ scripts/tmc-attach.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $TKG_LAB_SCRIPTS/set-env.sh
+source ./scripts/set-env.sh
 
 if [ ! $# -eq 1 ]; then
   echo "Must supply cluster name args"
