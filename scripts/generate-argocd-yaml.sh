d31aa9a13c6898c35b7c5a99f94f22c37a4a7c5b
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-07-01 17:46:38 -0400

    Update argo lab for helm deploy and contour ingress

---
+++ scripts/generate-argocd-yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+CLUSTER_NAME=$(yq r $PARAMS_YAML shared-services-cluster.name)
+ARGOCD_CN=$(yq r $PARAMS_YAML argocd.server-fqdn)
+ARGOCD_PASSWORD=$(yq r $PARAMS_YAML argocd.password)
+
+mkdir -p generated/$CLUSTER_NAME/argocd/
+cp argocd/*.yaml generated/$CLUSTER_NAME/argocd/
+
+yq write -d0 generated/$CLUSTER_NAME/argocd/httpproxy.yaml -i "spec.virtualhost.fqdn" $ARGOCD_CN
+yq write -d0 generated/$CLUSTER_NAME/argocd/values.yaml -i "configs.secret.argocdServerAdminPassword" `htpasswd -nbBC 10 "" $ARGOCD_PASSWORD | tr -d ':\n' | sed 's/$2y/$2a/'`
