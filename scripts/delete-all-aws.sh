1501a6965e5286eb44475dc43cfbdab57b3c3526
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-07-06 13:32:27 -0400

    Resolves tmc cluster name convention issue

--- scripts/delete-all-aws.sh
+++ scripts/delete-all-aws.sh
@@ -4,12 +4,12 @@ source ./scripts/set-env.sh
 
 VMWARE_ID=$(yq r $PARAMS_YAML vmware-id)
 
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML management-cluster.name) --force
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML shared-services-cluster.name) --force
+tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML management-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
+tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML shared-services-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
 
-tmc cluster namespace delete acme-fitness $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)
+tmc cluster namespace delete acme-fitness $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas)
 tmc workspace delete $(yq r $PARAMS_YAML acme-fitness.tmc-workspace)
-tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name) --force
+tmc cluster delete $VMWARE_ID-$(yq r $PARAMS_YAML workload-cluster.name)-$(yq r $PARAMS_YAML iaas) --force
 
 tkg set mc $(yq r $PARAMS_YAML management-cluster.name)
 tkg delete cluster $(yq r $PARAMS_YAML workload-cluster.name) --yes
