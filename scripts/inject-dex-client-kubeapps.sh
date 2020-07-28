109fc628615442795c767f3fc21a6748bddc3428
Dan Baskette <dbbaskette@gmail.com>
2020-07-28 11:04:48 -0400

    Initial Kubeapps Support
    Support for admin user and kubeapps.  All that's needed for client support are some additional instructions.

---
+++ scripts/inject-dex-client-kubeapps.sh
@@ -0,0 +1,25 @@
+# bin/bash -e
+
+## Adds additional path to the Shared Services DEX Entry for kubeapps OIDC
+
+MGMT_CLUSTER_NAME=$(yq r $PARAMS_YAML management-cluster.name)
+SVCS_CLUSTER_NAME=$(yq r $PARAMS_YAML shared-services-cluster.name)
+GANGWAY_CN=$(yq r $PARAMS_YAML shared-services-cluster.gangway-fqdn)
+KUBEAPPS="      - 'https://$(yq r $PARAMS_YAML kubeapps.server-fqdn)/oauth2/callback'"
+
+#Only inject if it is already there
+cat generated/$MGMT_CLUSTER_NAME/dex/04-cm.yaml | grep "id: $SVCS_CLUSTER_NAME"
+exists=$?
+if [ $exists -eq 0 ]; then
+    awk "1;/$GANGWAY_CN/{print \"$KUBEAPPS\"}" generated/$MGMT_CLUSTER_NAME/dex/04-cm.yaml > tmp && mv tmp generated/$MGMT_CLUSTER_NAME/dex/04-cm.yaml
+
+    kubectl config use-context $MGMT_CLUSTER_NAME-admin@$MGMT_CLUSTER_NAME
+    kubectl apply -f generated/$MGMT_CLUSTER_NAME/dex/04-cm.yaml
+    #force recycle of dex pod(s)
+    kubectl get po -n tanzu-system-auth
+    kubectl set env deployment dex --env="LAST_RESTART=$(date)" --namespace tanzu-system-auth
+    kubectl get po -n tanzu-system-auth
+    #switch back
+    kubectl config use-context $WORKLOAD_CLUSTER_NAME-admin@$WORKLOAD_CLUSTER_NAME
+fi
+
