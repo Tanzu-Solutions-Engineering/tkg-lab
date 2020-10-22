fb742280750a2785f4dd18bcccd1b9eefe25056e
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-10-22 10:35:17 -0400

    Add option to use S3 Storage to back Harbor

--- harbor/generate-and-apply-harbor-yaml.sh
+++ harbor/generate-and-apply-harbor-yaml.sh
@@ -33,6 +33,19 @@ yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "expose.ingress.ho
 yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "expose.ingress.hosts.notary" $NOTARY_CN  
 yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "externalURL" https://$HARBOR_CN
 
+# Check for Blob storage type
+HARBOR_BLOB_STORAGE_TYPE=$(yq r $PARAMS_YAML harbor.blob-storage.type)
+if [ "s3" == "$HARBOR_BLOB_STORAGE_TYPE" ]; then
+  yq d generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.persistentVolumeClaim"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.type" "s3"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.regionendpoint" "$(yq r $PARAMS_YAML harbor.blob-storage.regionendpoint)"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.region" "$(yq r $PARAMS_YAML harbor.blob-storage.region)"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.accesskey" "$(yq r $PARAMS_YAML harbor.blob-storage.access-key-id)"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.secretkey" "$(yq r $PARAMS_YAML harbor.blob-storage.secret-access-key)"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.bucket" "$(yq r $PARAMS_YAML harbor.blob-storage.bucket)"
+  yq write generated/$CLUSTER_NAME/harbor/harbor-values.yaml -i "persistence.imageChartStorage.s3.secure" "$(yq r $PARAMS_YAML harbor.blob-storage.secure)"
+fi
+
 helm repo add harbor https://helm.goharbor.io
 helm template harbor harbor/harbor -f generated/$CLUSTER_NAME/harbor/harbor-values.yaml --namespace harbor > generated/$CLUSTER_NAME/harbor/helm-manifest.yaml
 
