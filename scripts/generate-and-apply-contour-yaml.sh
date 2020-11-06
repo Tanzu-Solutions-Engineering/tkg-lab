ede1637f12e572b5fb8977d9df9758c4a85d18e4
jaimegag <jaimegag@gmail.com>
2020-11-06 01:58:45 -0500

    Use yq to customize Contour values

--- scripts/generate-and-apply-contour-yaml.sh
+++ scripts/generate-and-apply-contour-yaml.sh
@@ -11,14 +11,14 @@ IAAS=$(yq r $PARAMS_YAML iaas)
 
 kubectl config use-context $CLUSTER_NAME-admin@$CLUSTER_NAME
 
+mkdir -p generated/$CLUSTER_NAME/contour/
+
 # Go to the folder where we unpacked the extensions bundle
 cd tkg-extensions/extensions
 # Install the TMC Extensions Manager - Commented out since we already install it when attaching the cluster to TMC
 # kubectl apply -f tmc-extension-manager.yaml
 # Install the kapp Controller
-kubectl apply -f kapp-controller.yaml
-# Create a namespace and RBAC config for the Contour service
-kubectl apply -f ingress/contour/namespace-role.yaml
+##kubectl apply -f kapp-controller.yaml
 
 # Wait for kapp-controller pods to be Running
 while kubectl get pods -n vmware-system-tmc | grep kapp-controller | grep Running ; [ $? -ne 0 ]; do
@@ -26,25 +26,24 @@ while kubectl get pods -n vmware-system-tmc | grep kapp-controller | grep Runnin
 	sleep 5s
 done
 
+# Create a namespace and RBAC config for the Contour service
+##kubectl apply -f ingress/contour/namespace-role.yaml
+
 # Prepare Contour custom configuration
 if [ "$IAAS" = "aws" ];
 then
   # aws
-  cp ingress/contour/aws/contour-data-values.yaml.example ingress/contour/contour-data-values.yaml
+  yq read ingress/contour/aws/contour-data-values.yaml.example > ../../generated/$CLUSTER_NAME/contour/contour-data-values.yaml
 else
   # vsphere
-  cp ingress/contour/vsphere/contour-data-values.yaml.example ingress/contour/contour-data-values.yaml
-  cat >> ingress/contour/contour-data-values.yaml <<EOF
-
-  service:
-    type: LoadBalancer
-EOF
+  yq read ingress/contour/vsphere/contour-data-values.yaml.example > ../../generated/$CLUSTER_NAME/contour/contour-data-values.yaml
+  yq write -d0 ../../generated/$CLUSTER_NAME/contour/contour-data-values.yaml -i envoy.service.type LoadBalancer
 fi
 # Create secret with custom configuration
-kubectl create secret generic contour-data-values --from-file=values.yaml=ingress/contour/contour-data-values.yaml -n tanzu-system-ingress
+##kubectl create secret generic contour-data-values --from-file=values.yaml=../../generated/$CLUSTER_NAME/contour/contour-data-values.yaml -n tanzu-system-ingress
 
 # Deploy Contour Extension
-kubectl apply -f ingress/contour/contour-extension.yaml
+##kubectl apply -f ingress/contour/contour-extension.yaml
 
 # Wait until reconcile succeeds
 while kubectl get app contour -n tanzu-system-ingress | grep contour | grep "Reconcile succeeded" ; [ $? -ne 0 ]; do
