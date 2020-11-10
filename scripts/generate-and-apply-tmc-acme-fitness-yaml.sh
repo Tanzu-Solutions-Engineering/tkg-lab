fd2cf3c24d243134173f835bc66d457ea2df6e74
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-11-10 18:20:45 -0500

    Updates acme-fitness lab for changes in tmc api

--- scripts/generate-and-apply-tmc-acme-fitness-yaml.sh
+++ scripts/generate-and-apply-tmc-acme-fitness-yaml.sh
@@ -18,11 +18,11 @@ cp -r tmc/config/* generated/$CLUSTER_NAME/tmc/
 
 # acme-fitness-dev.yaml
 yq write -d0 generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml -i "fullName.name" $TMC_ACME_FITNESS_WORKSPACE_NAME
-yq write -d0 generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml -i "objectMeta.labels.origin" $VMWARE_ID
+yq write -d0 generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml -i "meta.labels.origin" $VMWARE_ID
 
 # tkg-wlc-acme-fitness.yaml
 yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "fullName.clusterName" $VMWARE_ID-$CLUSTER_NAME-$IAAS
-yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "objectMeta.labels.origin" $VMWARE_ID
+yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "meta.labels.origin" $VMWARE_ID
 yq write -d0 generated/$CLUSTER_NAME/tmc/namespace/tkg-wlc-acme-fitness.yaml -i "spec.workspaceName" $TMC_ACME_FITNESS_WORKSPACE_NAME
 
 tmc workspace create -f generated/$CLUSTER_NAME/tmc/workspace/acme-fitness-dev.yaml
