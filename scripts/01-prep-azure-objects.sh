e0cc6783ed0f32bb78a98c88b172b8a61c57d7ea
curtis <curtis@serverascode.com>
2020-11-18 18:20:00 +0000

    update to use az sp create-for-rbac

--- scripts/01-prep-azure-objects.sh
+++ scripts/01-prep-azure-objects.sh
@@ -16,42 +16,45 @@ if ! az account show > /dev/null; then
   exit 1
 fi
 
-# Get tenant id
-AZURE_TENANT_ID=$(yq r "$PARAMS_YAML" azure.tenant-id)
-if [ -z "$AZURE_TENANT_ID" ]; then 
-    AZURE_TENANT_ID=$(az account get-access-token --query tenant --output tsv)
-    yq write "$PARAMS_YAML" -i "azure.tenant-id" "$AZURE_TENANT_ID"
-fi
-echo "INFO: azure tenant id is $AZURE_TENANT_ID"
-
 # Get subscription id
 AZURE_SUBSCRIPTION_ID=$(yq r "$PARAMS_YAML" azure.subscription-id)
 if [ -z "$AZURE_SUBSCRIPTION_ID" ]; then
     AZURE_SUBSCRIPTION_ID=$(az account show --query id --output tsv)
-    yq write "$PARAMS_YAML" -i "azure.subscription-id" "$AZURE_SUBSCRIPTION_ID"
 fi
-echo "INFO: azure subscription id is $AZURE_SUBSCRIPTION_ID"
 
-# Set an app name - tkg-$date
+# Create an app name - tkg-$date
 AZURE_APP_NAME=$(yq r "$PARAMS_YAML" azure.app-name)
 if [ -z "$AZURE_APP_NAME" ]; then
   AZURE_APP_NAME=tkg-$(date +%F-%H-%M)
-  yq write "$PARAMS_YAML" -i "azure.app-name" "$AZURE_APP_NAME"
 fi
+
+# Create a service principle and app and store the returned json
+RETURNED_SP_APP_JSON=$(az ad sp create-for-rbac --name $AZURE_APP_NAME)
+
+# Setup variables from resulting json
+# NOTE(curtis): There are fancy ways to do this as well, but this works
+AZURE_CLIENT_ID=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.appId')
+AZURE_CLIENT_SECRET=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.password')
+AZURE_TENANT_ID=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.tenant')
+
+# Write resulting variables to params file
+
 echo "INFO: azure app name is $AZURE_APP_NAME"
+yq write "$PARAMS_YAML" -i "azure.app-name" "$AZURE_APP_NAME"
 
-# Generate a client secret to use when creating the azure app
-AZURE_CLIENT_SECRET=$(yq r "$PARAMS_YAML" azure.client-secret)
-if [ -z "$AZURE_CLIENT_SECRET" ]; then
-  AZURE_CLIENT_SECRET=$(openssl rand -base64 32)
-  yq write "$PARAMS_YAML" -i "azure.client-secret" "$AZURE_CLIENT_SECRET"
-fi
+echo "INFO: azure subscription id is $AZURE_SUBSCRIPTION_ID"
+yq write "$PARAMS_YAML" -i "azure.subscription-id" "$AZURE_SUBSCRIPTION_ID"
 
-# Create the azure app and get the client/app id
-AZURE_CLIENT_ID=$(az ad app create --display-name "$AZURE_APP_NAME" \
-  --homepage "http://$AZURE_APP_NAME" \
-  --identifier-uris "https://$AZURE_APP_NAME" \
-  --password "$AZURE_CLIENT_SECRET" | jq -r .appId)
+echo "INFO: azure tenant id is $AZURE_TENANT_ID"
+yq write "$PARAMS_YAML" -i "azure.tenant-id" "$AZURE_TENANT_ID"
 
-yq write "$PARAMS_YAML" -i azure.client-id "$AZURE_CLIENT_ID"
 echo "INFO: azure client id is $AZURE_CLIENT_ID"
+yq write "$PARAMS_YAML" -i azure.client-id "$AZURE_CLIENT_ID"
+
+echo "INFO: azure app name is $AZURE_APP_NAME"
+yq write "$PARAMS_YAML" -i "azure.app-name" "$AZURE_APP_NAME"
+
+yq write "$PARAMS_YAML" -i "azure.client-secret" "$AZURE_CLIENT_SECRET"
+#echo "INFO: password is $AZURE_CLIENT_SECRET"
+
+# done
