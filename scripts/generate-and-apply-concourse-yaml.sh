8872a6a2869b03528f349a74b25d7aace30d0828
Dodd Pfeffer <dpfeffer@pivotal.io>
2020-11-19 08:42:09 -0500

    Updates to concourse lab after running through tests.  Adds info to readme, and sample pipeline

--- scripts/generate-and-apply-concourse-yaml.sh
+++ scripts/generate-and-apply-concourse-yaml.sh
@@ -7,38 +7,33 @@ OKTA_AUTH_SERVER_CN=$(yq r $PARAMS_YAML okta.auth-server-fqdn)
 OKTA_AUTH_SERVER_CA_CERT="$(cat keys/letsencrypt-ca.pem)"
 OKTA_CONCOURSE_APP_CLIENT_ID=$(yq r $PARAMS_YAML okta.concourse-app-client-id)
 OKTA_CONCOURSE_APP_CLIENT_SECRET=$(yq r $PARAMS_YAML okta.concourse-app-client-secret)
+ADMIN_PASSWORD=$(yq r $PARAMS_YAML concourse.admin-password)
 
 mkdir -p generated/$CLUSTER_NAME/concourse/
 
 cp concourse/concourse-values-contour-template.yaml generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
 
-if [ `uname -s` = 'Darwin' ]; 
-then
-  sed -i '' -e "s/CONCOURSE_URL/$CONCOURSE_URL/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i '' -e "s/OKTA_AUTH_SERVER_CN/$OKTA_AUTH_SERVER_CN/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i '' -e "s/OKTA_CONCOURSE_APP_CLIENT_ID/$OKTA_CONCOURSE_APP_CLIENT_ID/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i '' -e "s/OKTA_CONCOURSE_APP_CLIENT_SECRET/$OKTA_CONCOURSE_APP_CLIENT_SECRET/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-else
-  sed -i -e "s/CONCOURSE_URL/$CONCOURSE_URL/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i -e "s/OKTA_AUTH_SERVER_CN/$OKTA_AUTH_SERVER_CN/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i -e "s/$OKTA_AUTH_SERVER_CN/$OKTA_AUTH_SERVER_CN/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i -e "s/OKTA_CONCOURSE_APP_CLIENT_ID/$OKTA_CONCOURSE_APP_CLIENT_ID/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-  sed -i -e "s/OKTA_CONCOURSE_APP_CLIENT_SECRET/$OKTA_CONCOURSE_APP_CLIENT_SECRET/g" generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml
-fi
-
 yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "secrets.oidcCaCert" -- "${OKTA_AUTH_SERVER_CA_CERT}"
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "secrets.localUsers" "admin:$ADMIN_PASSWORD"
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "web.ingress.hosts[0]" $CONCOURSE_URL
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "web.ingress.tls[0].hosts[0]" $CONCOURSE_URL
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "concourse.web.externalUrl" https://$CONCOURSE_URL
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "concourse.web.auth.oidc.issuer" https://$OKTA_AUTH_SERVER_CN/oauth2/default
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "secrets.oidcClientId" $OKTA_CONCOURSE_APP_CLIENT_ID
+yq write -d0 generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml -i "secrets.oidcClientSecret" $OKTA_CONCOURSE_APP_CLIENT_SECRET
 
 # generate the helm manifest and make sure the web pod trusts let's encrypt
 helm repo add concourse https://concourse-charts.storage.googleapis.com/
 helm repo update
 
+# NOTE: Through testing setting the OIDC CA in values.yaml did not work as expected, so we are mounting the let's encrypt CA onto the worker pods via ytt overlay
 helm template concourse concourse/concourse -f generated/$CLUSTER_NAME/concourse/concourse-values-contour.yaml --namespace $CONCOURSE_NAMESPACE | 
   ytt -f - -f overlay/trust-certificate --ignore-unknown-comments \
     --data-value certificate="$(cat keys/letsencrypt-ca.pem)" \
     --data-value ca=letsencrypt > generated/$CLUSTER_NAME/concourse/helm-manifest.yaml
 
 kapp deploy -a concourse  \
-  -n tanzu-kapp \
-  --into-ns $CONCOURSE_NAMESPACE \
   -f generated/$CLUSTER_NAME/concourse/helm-manifest.yaml \
+  -n $CONCOURSE_NAMESPACE \
+  -y
  
