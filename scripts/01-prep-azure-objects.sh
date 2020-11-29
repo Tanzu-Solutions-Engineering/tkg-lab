80bbabead6ac48b06e7a2e4653f7693db9d211ca
curtis <curtis@serverascode.com>
2020-11-29 12:31:12 +0000

    add generate ssh key to azure object prep

--- scripts/01-prep-azure-objects.sh
+++ scripts/01-prep-azure-objects.sh
@@ -3,6 +3,11 @@
 TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$TKG_LAB_SCRIPTS/set-env.sh"
 
+#
+# Setup and preflight checks
+#
+
+
 # Check if client-id is already set, if so exit 
 AZURE_CLIENT_ID=$(yq r "$PARAMS_YAML" azure.client-id)
 if [ -n "$AZURE_CLIENT_ID" ]; then
@@ -15,6 +20,19 @@ if ! az account show > /dev/null; then
   echo "ERROR: could not run az account show, please configure az"
   exit 1
 fi
+
+if [ "$TKG_CONFIG" = "" ]; then
+  TKG_CONFIG=~/.tkg/config.yaml
+fi
+
+if [ ! -f "$TKG_CONFIG" ]; then
+  # Create ~/.tkg
+  tkg get mc > /dev/null
+fi
+
+#
+# Configure
+#
 
 # Get subscription id
 AZURE_SUBSCRIPTION_ID=$(yq r "$PARAMS_YAML" azure.subscription-id)
@@ -37,7 +55,24 @@ AZURE_CLIENT_ID=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.appId')
 AZURE_CLIENT_SECRET=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.password')
 AZURE_TENANT_ID=$(echo "$RETURNED_SP_APP_JSON" | jq -r '.tenant')
 
+# Create an SSH key for use
+# Create SSH key
+mkdir -p keys/
+tkg_key_file="./keys/tkg_rsa"
+echo "INFO: checking for existing SSH key at $tkg_key_file: "
+if [ -f "$tkg_key_file" ]; then
+  echo "INFO: skipping ssh key generation"
+else
+  echo "INFO: generating ssh key at $tkg_key_file"
+  ssh-keygen -t rsa -b 4096 -f "$tkg_key_file" -q -N ""
+fi
+
+#
 # Write resulting variables to params file
+#
+
+echo "INFO: writing ssh key to config file"
+yq write $TKG_CONFIG -i "AZURE_SSH_PUBLIC_KEY_B64" "$(base64 < "$tkg_key_file" | tr -d '\r\n')"
 
 echo "INFO: azure app name is $AZURE_APP_NAME"
 yq write "$PARAMS_YAML" -i "azure.app-name" "$AZURE_APP_NAME"
