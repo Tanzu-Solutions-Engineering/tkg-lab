2a1c52079cb3877b99c1725674ca17f60316128d
DongHee Lee <TheKoguryo@gmail.com>
2020-12-19 18:40:04 +0900

    Update 01-prep-aws-objects.sh
    'tkg config permissions aws' needs AWS env values.

--- scripts/01-prep-aws-objects.sh
+++ scripts/01-prep-aws-objects.sh
@@ -3,13 +3,13 @@
 TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $TKG_LAB_SCRIPTS/set-env.sh
 
-tkg config permissions aws
-
 export AWS_REGION=$(yq r $PARAMS_YAML aws.region)
 export AWS_ACCESS_KEY_ID=$(yq r $PARAMS_YAML aws.access-key-id)
 export AWS_SECRET_ACCESS_KEY=$(yq r $PARAMS_YAML aws.secret-access-key)
 TKG_ENVIRONMENT_NAME=$(yq r $PARAMS_YAML environment-name)
 SSH_KEY_FILE_NAME=$TKG_ENVIRONMENT_NAME-ssh.pem
+
+tkg config permissions aws
 
 mkdir -p keys/
 if [[ ! -f ./keys/$SSH_KEY_FILE_NAME ]]; then
