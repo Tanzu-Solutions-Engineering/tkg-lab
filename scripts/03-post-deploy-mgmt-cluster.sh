4691ccd4f881515af432d6989d5fb118cf62c6fe
Dodd Pfeffer <dpfeffer@pivotal.io>
2021-03-08 16:54:09 -0500

    Updates readme for managment cluster AWS and associated scripts

--- scripts/03-post-deploy-mgmt-cluster.sh
+++ scripts/03-post-deploy-mgmt-cluster.sh
@@ -4,9 +4,14 @@ TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 source $TKG_LAB_SCRIPTS/set-env.sh
 
 MANAGEMENT_CLUSTER_NAME=$(yq e .management-cluster.name $PARAMS_YAML)
+WORKER_REPLICAS=$(yq e .management-cluster.worker-replicas $PARAMS_YAML)
+
+tanzu cluster scale MANAGEMENT_CLUSTER_NAME -n tkg-system -w $WORKER_REPLICAS
 
 kubectl config use-context $MANAGEMENT_CLUSTER_NAME-admin@$MANAGEMENT_CLUSTER_NAME
 
+# We have found that after the tanzu cli reports that the managmement cluster is created, there are additional initialation of system pods.  In order 
+# to ensure that the cluster is fully initilized, we will wait for the pinniped-supervisor job to be completed.
 while kubectl get po -n pinniped-supervisor | grep Completed ; [ $? -ne 0 ]; do
 	echo "Pinniped Configuration is not yet complete"
 	sleep 5s
