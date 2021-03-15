11d8686dd8112de5fd17ef46448bb72eeedb498a
Dodd Pfeffer <dpfeffer@pivotal.io>
2021-03-15 16:21:17 -0400

    Changes export variable names to avoid any confusion if cluster-config.yaml writes fail

--- scripts/02-deploy-aws-mgmt-cluster.sh
+++ scripts/02-deploy-aws-mgmt-cluster.sh
@@ -3,24 +3,24 @@
 TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $TKG_LAB_SCRIPTS/set-env.sh
 
-export CLUSTER_NAME=$(yq e .management-cluster.name $PARAMS_YAML)
+export CLUSTER=$(yq e .management-cluster.name $PARAMS_YAML)
 
-mkdir -p generated/$CLUSTER_NAME
-cp config-templates/aws-cluster-config.yaml generated/$CLUSTER_NAME/cluster-config.yaml
+mkdir -p generated/$CLUSTER
+cp config-templates/aws-cluster-config.yaml generated/$CLUSTER/cluster-config.yaml
 
 export REGION=$(yq e .aws.region $PARAMS_YAML)
-export AWS_SSH_KEY_NAME=tkg-$(yq e .environment-name $PARAMS_YAML)-default
-export OIDC_IDENTITY_PROVIDER_ISSUER_URL=https://$(yq e .okta.auth-server-fqdn $PARAMS_YAML)
-export OIDC_IDENTITY_PROVIDER_CLIENT_ID=$(yq e .okta.dex-app-client-id $PARAMS_YAML)
-export OIDC_IDENTITY_PROVIDER_CLIENT_SECRET=$(yq e .okta.dex-app-client-secret $PARAMS_YAML)
+export SSH_KEY_NAME=tkg-$(yq e .environment-name $PARAMS_YAML)-default
+export OIDC_ISSUER_URL=https://$(yq e .okta.auth-server-fqdn $PARAMS_YAML)
+export OIDC_CLIENT_ID=$(yq e .okta.dex-app-client-id $PARAMS_YAML)
+export OIDC_CLIENT_SECRET=$(yq e .okta.dex-app-client-secret $PARAMS_YAML)
 export WORKER_REPLICAS=$(yq e .management-cluster.worker-replicas $PARAMS_YAML)
 
-yq e -i '.CLUSTER_NAME = env(CLUSTER_NAME)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.AWS_REGION = env(REGION)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.AWS_SSH_KEY_NAME = env(AWS_SSH_KEY_NAME)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.OIDC_IDENTITY_PROVIDER_ISSUER_URL = env(OIDC_IDENTITY_PROVIDER_ISSUER_URL)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.OIDC_IDENTITY_PROVIDER_CLIENT_ID = env(OIDC_IDENTITY_PROVIDER_CLIENT_ID)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.OIDC_IDENTITY_PROVIDER_CLIENT_SECRET = env(OIDC_IDENTITY_PROVIDER_CLIENT_SECRET)' generated/$CLUSTER_NAME/cluster-config.yaml
-yq e -i '.WORKER_MACHINE_COUNT = env(WORKER_REPLICAS)' generated/$CLUSTER_NAME/cluster-config.yaml
+yq e -i '.CLUSTER_NAME = env(CLUSTER)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.AWS_REGION = env(REGION)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.AWS_SSH_KEY_NAME = env(SSH_KEY_NAME)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.OIDC_IDENTITY_PROVIDER_ISSUER_URL = env(OIDC_ISSUER_URL)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.OIDC_IDENTITY_PROVIDER_CLIENT_ID = env(OIDC_CLIENT_ID)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.OIDC_IDENTITY_PROVIDER_CLIENT_SECRET = env(OIDC_CLIENT_SECRET)' generated/$CLUSTER/cluster-config.yaml
+yq e -i '.WORKER_MACHINE_COUNT = env(WORKER_REPLICAS)' generated/$CLUSTER/cluster-config.yaml
 
-tanzu management-cluster create --file=generated/$CLUSTER_NAME/cluster-config.yaml -v 6
+tanzu management-cluster create --file=generated/$CLUSTER/cluster-config.yaml -v 6
