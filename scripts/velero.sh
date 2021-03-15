ae9b9fa3f37d3b6314eebdea583c462559b4265f
Dodd Pfeffer <dpfeffer@pivotal.io>
2021-03-15 16:15:40 -0400

    Updates velero script given tkg secret fix that came in 1.3

--- scripts/velero.sh
+++ scripts/velero.sh
@@ -19,48 +19,18 @@ IAAS=$(yq e .iaas $PARAMS_YAML)
 
 if [ "$IAAS" = "vsphere" ];
 then
-
-  # Begin Hack
-  # The following hack is required because the vSphere Plugin is looking for a secret with the name “vsphere-config-secret“. This secret contains 
-  # the vCenter Server credentials and config. In TKG, the secret used has a different name, and therefore cannot be found.
-  # this is currently under review with the vSphere Plugin team to see if we can address this issue.
-  # Nevertheless, there is an easy workaround for it, simply create another secret with the name “vsphere-config-secret” and the required information.
-  # See https://beyondelastic.com/2020/04/30/backup-and-migrate-tkgi-pks-to-tkg-with-velero/ for where this issue was identified
-  VSPHERE_SERVER=$(yq e .vsphere.server $PARAMS_YAML)
-  VSPHERE_USERNAME=$(yq e .vsphere.username $PARAMS_YAML)
-  VSPHERE_PASSWORD=$(yq e .vsphere.password $PARAMS_YAML)
-
-  mkdir -p generated/$CLUSTER_NAME/velero/
-
-  cat > generated/$CLUSTER_NAME/velero/csi-vsphere.conf <<EOF
-[Global]
-cluster-id = "$CLUSTER_NAME"
-
-[VirtualCenter "$VSPHERE_SERVER"]
-insecure-flag = "true"
-user = "$VSPHERE_USERNAME"
-password = "$VSPHERE_PASSWORD"
-port = "443"
-EOF
-
-  kubectl create secret generic vsphere-config-secret \
-    --from-file=generated/$CLUSTER_NAME/velero/csi-vsphere.conf \
-    --namespace=kube-system
-  # End Hack
-
   velero install \
       --provider aws \
-      --plugins velero/velero-plugin-for-aws:v1.0.1,vsphereveleroplugin/velero-plugin-for-vsphere:1.0.0 \
+      --plugins velero/velero-plugin-for-aws:v1.1.0,vsphereveleroplugin/velero-plugin-for-vsphere:1.1.0 \
       --bucket $VELERO_BUCKET \
       --backup-location-config region=$VELERO_REGION \
       --snapshot-location-config region=$VELERO_REGION \
       --secret-file keys/credentials-velero
   velero snapshot-location create vsl-vsphere --provider velero.io/vsphere
-
 else
   velero install \
       --provider aws \
-      --plugins velero/velero-plugin-for-aws:v1.0.1 \
+      --plugins velero/velero-plugin-for-aws:v1.1.0 \
       --bucket $VELERO_BUCKET \
       --backup-location-config region=$VELERO_REGION \
       --snapshot-location-config region=$VELERO_REGION \
