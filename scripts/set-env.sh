5577df02a9653c2efb7ce4187654325fc72c8266
doddatpivotal <dpfeffer@vmware.com>
2021-08-05 09:00:01 -0400

    Updates yq workaround to test prior to adding doc seperator

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -5,13 +5,20 @@
 # Give some information timestamps to know how long things take
 echo $(date)
 
+# YQ versions before 4.11.1 would remove document seperator when updating docs
+# the issue has been fixed, but keeping this function in place to handle either condition
 add_yaml_doc_seperator() {
-if [ `uname -s` = 'Darwin' ];
-then
-  sed -i '' '3i\
+if grep "\-\-\-" $1; then  
+  echo "Skipping, adding document seperator"
+else
+  echo "Adding document seperator"
+  if [ `uname -s` = 'Darwin' ];
+  then
+    sed -i '' '3i\
 ---\
 ' $1
-else
-  sed -i -e '3i\---\' $1
+  else
+    sed -i -e '3i\---\' $1
+  fi
 fi
 }
