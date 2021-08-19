b9c9e089bdf8a63c5876233d55fd369d9459dde5
doddatpivotal <dpfeffer@vmware.com>
2021-08-19 01:19:50 +0000

    Completes base lab refactors for dakar

--- scripts/velero.sh
+++ scripts/velero.sh
@@ -20,13 +20,13 @@ IAAS=$(yq e .iaas $PARAMS_YAML)
 if [ "$IAAS" = "vsphere" ];
 then
   velero install \
+      --image=projects-stg.registry.vmware.com/tkg/velero/velero:v1.6.2_vmware.1 \
       --provider aws \
-      --plugins velero/velero-plugin-for-aws:v1.1.0,vsphereveleroplugin/velero-plugin-for-vsphere:1.1.0 \
+      --plugins=projects-stg.registry.vmware.com/tkg/velero/velero-plugin-for-aws:v1.2.1_vmware.1,projects-stg.registry.vmware.com/tkg/velero/velero-plugin-for-vsphere:v1.1.1_vmware.1 \
       --bucket $VELERO_BUCKET \
       --backup-location-config region=$VELERO_REGION \
       --snapshot-location-config region=$VELERO_REGION \
       --secret-file keys/credentials-velero
-  velero snapshot-location create vsl-vsphere --provider velero.io/vsphere
 else
   velero install \
       --provider aws \
