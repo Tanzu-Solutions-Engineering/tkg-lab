f2c9210bc850694c9700d82138257c81d75a70b8
doddatpivotal <dpfeffer@vmware.com>
2021-11-02 20:25:42 -0400

    Simplifies pinniped secret update logic

--- scripts/update-pinniped-configuration.sh
+++ scripts/update-pinniped-configuration.sh
@@ -49,13 +49,11 @@ yq e -i '.pinniped.supervisor_svc_external_dns = env(PINNIPED_CN)' generated/$CL
 yq e -i '.pinniped.supervisor_ca_bundle_data = env(CA_BUNDLE)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
 yq e -i '.pinniped.supervisor_svc_endpoint = env(PINNIPED_SVC_ENDPOINT)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
 
-# Deleting the existing job.  I will be recreated when the pinniped-addon secret is updated below.  And then gives us a chance to wait until job is competed
+# Deleting the existing job.  It will be recreated when the pinniped-addon secret is updated below.  And then gives us a chance to wait until job is competed
 kubectl delete job pinniped-post-deploy-job -n pinniped-supervisor
 
-kubectl create secret generic $CLUSTER_NAME-pinniped-addon --from-file=values.yaml=generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml -n tkg-system -o yaml --type=tkg.tanzu.vmware.com/addon --dry-run=client | kubectl apply -f-
-kubectl annotate secret $CLUSTER_NAME-pinniped-addon --overwrite -n tkg-system tkg.tanzu.vmware.com/addon-type=authentication/pinniped
-kubectl label secret $CLUSTER_NAME-pinniped-addon --overwrite -n tkg-system tkg.tanzu.vmware.com/addon-name=pinniped
-kubectl label secret $CLUSTER_NAME-pinniped-addon --overwrite -n tkg-system tkg.tanzu.vmware.com/cluster-name=$CLUSTER_NAME
+NEW_VALUES=`cat generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml | base64`
+kubectl patch secret $CLUSTER_NAME-pinniped-addon -n tkg-system -p '{"data": {"values.yaml": "'$NEW_VALUES'"}}'
 
 # Wait until job is completed.
 while kubectl get jobs -n pinniped-supervisor | grep "1/1"; [ $? -ne 0 ]; do
