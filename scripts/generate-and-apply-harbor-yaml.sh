cbb3c3601f251c60aca370882cc8f4c9e32815e0
jaimegag <jaimegag@gmail.com>
2021-12-14 18:29:41 -0500

    Fix Harbor S3 Storage for tkg 1.4.1

--- scripts/generate-and-apply-harbor-yaml.sh
+++ scripts/generate-and-apply-harbor-yaml.sh
@@ -105,10 +105,14 @@ tanzu package install harbor \
 kubectl create secret generic harbor-notary-singer-image-overlay -n tanzu-kapp -o yaml --dry-run=client --from-file=tkg-extensions-mods-examples/registry/harbor/overlay-notary-signer-image-fix.yaml | kubectl apply -f -
 kubectl annotate PackageInstall harbor -n tanzu-kapp ext.packaging.carvel.dev/ytt-paths-from-secret-name.0=harbor-notary-singer-image-overlay
 
-# Patch (via overlay) the harbor-registry to use an empty-dir for registry-data when using S3 storage
+# Patch (via overlay) the harbor-registry when using S3 storage. This won't be necessary on TKG 1.5
+# - create harbor-registry secret
+# - use an empty-dir for registry-data to clean PVC info
 if [ "s3" == "$HARBOR_BLOB_STORAGE_TYPE" ]; then
+  kubectl create secret generic harbor-storage-overlay -n tanzu-kapp -o yaml --dry-run=client --from-file=tkg-extensions-mods-examples/registry/harbor/overlay-storage-fix.yaml | kubectl apply -f -
+  kubectl annotate PackageInstall harbor -n tanzu-kapp ext.packaging.carvel.dev/ytt-paths-from-secret-name.1=harbor-storage-overlay
   kubectl create secret generic harbor-s3-overlay -n tanzu-kapp -o yaml --dry-run=client --from-file=tkg-extensions-mods-examples/registry/harbor/overlay-s3-pvc-fix.yaml | kubectl apply -f-
-  kubectl annotate PackageInstall harbor -n tanzu-kapp ext.packaging.carvel.dev/ytt-paths-from-secret-name.0=harbor-s3-overlay
+  kubectl annotate PackageInstall harbor -n tanzu-kapp ext.packaging.carvel.dev/ytt-paths-from-secret-name.2=harbor-s3-overlay
 fi
 
 # Wait for the Package to reconcile
