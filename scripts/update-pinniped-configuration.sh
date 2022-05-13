0ffaf6ca6f550fa09676e9b50570e6ca08576521
jaimegag <jaimegag@gmail.com>
2022-05-12 23:25:43 -0400

    align pinniped configuration to 1.5.3

--- scripts/update-pinniped-configuration.sh
+++ scripts/update-pinniped-configuration.sh
@@ -5,6 +5,7 @@ source $TKG_LAB_SCRIPTS/set-env.sh
 
 CLUSTER_NAME=$(yq e .management-cluster.name $PARAMS_YAML)
 export PINNIPED_CN=$(yq e .management-cluster.pinniped-fqdn $PARAMS_YAML)
+export PINNIPED_ENDPOINT=https://$PINNIPED_CN
 
 kubectl config use-context $CLUSTER_NAME-admin@$CLUSTER_NAME
 
@@ -30,9 +31,6 @@ while kubectl get certificate custom-pinniped-cert -n pinniped-supervisor | grep
 	sleep 5
 done
 
-kubectl get secret $CLUSTER_NAME-pinniped-addon -n tkg-system -ojsonpath="{.data.values\.yaml}" | base64 --decode > generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
-
-export PINNIPED_SVC_ENDPOINT=https://$PINNIPED_CN
 if [ `uname -s` = 'Darwin' ];
 then
 	export CA_BUNDLE=`cat keys/letsencrypt-ca.pem | base64`
@@ -40,10 +38,20 @@ else
 	export CA_BUNDLE=`cat keys/letsencrypt-ca.pem | base64 -w 0`
 fi
 
-yq e -i '.custom_tls_secret = "custom-auth-cert-tls"' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
-yq e -i '.pinniped.supervisor_svc_external_dns = env(PINNIPED_CN)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
-yq e -i '.pinniped.supervisor_ca_bundle_data = env(CA_BUNDLE)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
-yq e -i '.pinniped.supervisor_svc_endpoint = env(PINNIPED_SVC_ENDPOINT)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
+# Copy secret generated from certificate to a new one to combine both cert and CA data
+kubectl get secret custom-auth-cert-tls -n pinniped-supervisor -oyaml > generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i '.metadata.name = "custom-auth-cert-tls-with-ca"' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i '.data."ca.crt" = strenv(CA_BUNDLE)' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i 'del(.metadata.annotations)' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i 'del(.metadata.creationTimestamp)' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i 'del(.metadata.uid)' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+yq e -i 'del(.metadata.resourceVersion)' generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+kubectl apply -f generated/$CLUSTER_NAME/pinniped/pinniped-secret-with-ca.yaml
+
+# Update add-on values
+kubectl get secret $CLUSTER_NAME-pinniped-addon -n tkg-system -ojsonpath="{.data.values\.yaml}" | base64 --decode > generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
+yq e -i '.custom_tls_secret = "custom-auth-cert-tls-with-ca"' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
+yq e -i '.pinniped.supervisor_svc_external_dns = env(PINNIPED_ENDPOINT)' generated/$CLUSTER_NAME/pinniped/pinniped-addon-values.yaml
 
 # Deleting the existing job.  It will be recreated when the pinniped-addon secret is updated below.  And then gives us a chance to wait until job is competed
 kubectl delete job pinniped-post-deploy-job -n pinniped-supervisor
@@ -63,31 +71,3 @@ while kubectl get jobs -n pinniped-supervisor | grep "1/1"; [ $? -ne 0 ]; do
 	echo "Waiting for pinniped-post-deploy-job job to be completed"
 	sleep 5
 done
-
-# Now patch the CRDs - This is a hack because there is no way to configure this information
-
-kubectl patch federationdomain pinniped-federation-domain \
-	-n pinniped-supervisor \
-	--type json \
-	-p="[{'op': 'replace', 'path': '/spec/issuer', 'value':$PINNIPED_SVC_ENDPOINT}]"
-
-# Validate: k get federationdomain -n pinniped-supervisor -oyaml
-
-# Validate: k get oidcidentityprovider -n pinniped-supervisor -oyaml
-# Get CA: k get oidcidentityprovider -n pinniped-supervisor -ojsonpath="{.items[0].spec.tls.certificateAuthorityData}" | base64 --decode
-
-kubectl patch jwtauthenticator tkg-jwt-authenticator \
-	-n pinniped-concierge \
-	--type json \
-	-p="[{'op': 'replace', 'path': '/spec/issuer', 'value':$PINNIPED_SVC_ENDPOINT},{'op': 'replace', 'path': '/spec/audience', 'value':$PINNIPED_SVC_ENDPOINT},{'op': 'replace', 'path': '/spec/tls/certificateAuthorityData', 'value':$CA_BUNDLE}]"
-# Validate: k get jwtauthenticator -n pinniped-concierge -oyaml
-# Get CA k get jwtauthenticator -n pinniped-concierge -ojsonpath="{.items[0].spec.tls.certificateAuthorityData}" | base64 --decode
-
-# Validate: k get jwtauthenticator,federationdomain,oidcidentityprovider -A
-
-kubectl patch cm pinniped-info \
-	-n kube-public \
-	--type json \
-	-p="[{'op': 'replace', 'path': '/data/issuer', 'value':$PINNIPED_SVC_ENDPOINT},{'op': 'replace', 'path': '/data/issuer_ca_bundle_data', 'value':$CA_BUNDLE}]"
-# Validate: k get cm pinniped-info -n kube-public -oyaml
-# Get CA: k get cm pinniped-info -n kube-public -ojsonpath="{.data.issuer_ca_bundle_data}" | base64 --decode
