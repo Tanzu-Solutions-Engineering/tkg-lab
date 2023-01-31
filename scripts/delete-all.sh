2bb53ced34a751ef5f8b24f185d0f81be45c9059
doddatpivotal <dpfeffer@vmware.com>
2023-01-31 17:53:02 -0500

    Adds terraform destroy to delete script for aws

--- scripts/delete-all.sh
+++ scripts/delete-all.sh
@@ -5,6 +5,7 @@ source $TKG_LAB_SCRIPTS/set-env.sh
 
 VMWARE_ID=$(yq e .vmware-id $PARAMS_YAML)
 MC_CLUSTER_NAME=$(yq e .management-cluster.name $PARAMS_YAML)
+IAAS=$(yq e .iaas $PARAMS_YAML)
 
 tmc cluster delete $VMWARE_ID-$(yq e .shared-services-cluster.name $PARAMS_YAML)-$(yq e .iaas $PARAMS_YAML) -m attached -p attached --force
 tmc cluster delete $VMWARE_ID-$(yq e .workload-cluster.name $PARAMS_YAML)-$(yq e .iaas $PARAMS_YAML) -m attached -p attached  --force
@@ -19,8 +20,14 @@ while tanzu cluster list | grep deleting ; [ $? -eq 0 ]; do
 	sleep 5
 done   
 
-# HACK: Kubeconfig should not be required, OLYMP-26147 has been created address this.  Set current context to managment cluster
 kubectl config use-context $MC_CLUSTER_NAME-admin@$MC_CLUSTER_NAME
 tmc managementcluster deregister $MC_CLUSTER_NAME --force --kubeconfig ~/.kube/config
 
 tanzu management-cluster delete -y
+
+if [ "$IAAS" = "aws" ];
+then
+
+terraform -chdir=terraform/aws destroy -y
+
+fi
