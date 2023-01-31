4c988c1974e84541b1058f58afe9b03ddfaaafa8
Jaime Gonzalez Aguilar <jaimegag@gmail.com>
2023-01-31 11:28:34 -0500

    Updated md for MC and added Tanzu Standard Repo to it

--- scripts/03-post-deploy-mgmt-cluster.sh
+++ scripts/03-post-deploy-mgmt-cluster.sh
@@ -4,7 +4,6 @@ TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 source $TKG_LAB_SCRIPTS/set-env.sh
 
 MANAGEMENT_CLUSTER_NAME=$(yq e .management-cluster.name $PARAMS_YAML)
-WORKER_REPLICAS=$(yq e .management-cluster.worker-replicas $PARAMS_YAML)
 
 kubectl config use-context $MANAGEMENT_CLUSTER_NAME-admin@$MANAGEMENT_CLUSTER_NAME
 
@@ -14,6 +13,6 @@ while [[ $(kubectl get apps -n tkg-system -oyaml | yq e '.items[] | select(.stat
 	sleep 5
 done
 
-tanzu cluster scale $MANAGEMENT_CLUSTER_NAME -n tkg-system -w $WORKER_REPLICAS
-
+# Create namespace and Package Repository for User Managed Packages
 kubectl create ns tanzu-user-managed-packages --dry-run=client --output yaml | kubectl apply -f -
+$TKG_LAB_SCRIPTS/scripts/deploy-tanzu-standard-package-repo.sh $MANAGEMENT_CLUSTER_NAME
